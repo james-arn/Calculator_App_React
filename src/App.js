3fe229be02cf979f0aad5e0ab6cb7ce6e0afb935
James Arnold <j1mesu@hotmail.co.uk>
2021-10-22 11:33:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { create, all } from 'mathjs'
 
-function App() {
+const App = ()=>{
+  // collection of tasks, need an array.
+  const [input, setInput] = useState("")
+
+  const buttons = [
+    "7", "8", "9", "C",
+    "4", "5", "6", "/",
+    "1", "2", "3", "-",
+    "0", "+", "*", "="
+  ]
+
+  const addHandler = (button) =>{
+console.log(button)
+let newArr = [...input]
+newArr.push(input)
+console.log(newArr)
+setInput(newArr)
+// math.evaluate(newArr)
+}
+//concat untilclick equals which will then evaluate (add). Then the c will clear string.
+// IF user todenst lcick on c or equals then it adds to input.
+
+
+  
+  // math.evaluate('12 /')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Calculator</h1>
+    {buttons.map ((item, index)=>{
+      return (
+      <button key={index} value={item} onClick={()=>addHandler(item)} >{item}</button>
+    )}) }
     </div>
-  );
+  )
 }
 
 export default App;
